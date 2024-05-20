4ee90373e84af981662c4501521ada92c92412ca
Samer Munawwar <samer.mnr@gmail.com>
2024-05-20 16:29:15 +0200

    Personalregister app

--- restaurant/Program.cs
+++ restaurant/Program.cs
@@ -1,11 +1,53 @@
-﻿namespace Restaurant
+﻿using System;
+using System.Collections.Generic;
+
+// fråga.1 workerClass
+//fråga.2 workers list, Name, Salary.
+namespace Restaurant
 {
+    public class WorkerClass
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("Hello world! 2");
+            var workers = new List<WorkerClass>();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add a new worker");
+                Console.WriteLine("2. Stop");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Write the name: ");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("Write the salary: ");
+                    decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                    workers.Add(new WorkerClass { Name = name, Salary= salary });
+                }
+                else if (choice == 2)
+                {
+                    foreach (var worker in workers)
+                    {
+                        Console.WriteLine($"The worker name is {worker.Name} and the salary is {worker.Salary}kr");
+                    }
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"please try again.");
+                }
+
+            }
         }
     }
 }
